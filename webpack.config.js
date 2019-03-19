c2ad39e33de016fd14fb96a8860bf6abdf9497a7
MertNacar <mertnacar2374@gmail.com>
2019-03-19 11:31:15 +0300

    added .babelrc and config

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  output: {
+    publicPath: '/'                   
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+    port: 8080,
+    historyApiFallback: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./public/index.html",
+      filename: "./index.html"
+    })
+  ]
+};
